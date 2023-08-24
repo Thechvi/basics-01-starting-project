9fab0485fa9b242e90d35dfb2fd4fc037b689f02
Vitali Chabatar <thechvi@gmail.com>
2023-08-24 19:45:48 +0300

    new calc Maximilian

---
+++ assets/scripts/app.js
@@ -0,0 +1,52 @@
+let defaultResult = 0;
+let currentResult = defaultResult;
+
+function getUserNumberInput() {
+	return parseInt(usrInput.value);
+};
+
+function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
+	const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+	outputResult(currentResult, calcDescription);
+};
+
+function add() {
+	const enteredNumber = getUserNumberInput();
+	const initialResult = currentResult;
+	currentResult = currentResult + enteredNumber;
+	createAndWriteOutput('+', initialResult, enteredNumber)
+};
+
+function subtract() {
+	const enteredNumber = getUserNumberInput();
+	const initialResult = currentResult;
+	currentResult = currentResult - enteredNumber;
+	createAndWriteOutput('-', initialResult, enteredNumber)
+};
+
+function multiply() {
+	const enteredNumber = getUserNumberInput();
+	const initialResult = currentResult;
+	currentResult = currentResult * enteredNumber;
+	createAndWriteOutput('*', initialResult, enteredNumber)
+};
+
+function divide() {
+	const enteredNumber = getUserNumberInput();
+	const initialResult = currentResult;
+	currentResult = currentResult / enteredNumber;
+	createAndWriteOutput('/', initialResult, enteredNumber)
+};
+function reset() {
+
+	location.reload();
+
+};
+
+addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
+resetBtn.addEventListener('click', reset);
+
+
